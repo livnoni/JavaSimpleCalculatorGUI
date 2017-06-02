47fd0aced36bf0c62fc1c2ba19b7541dca1977fe
livnoni <yehuda.livnoni@gmail.com>
2017-06-02 13:41:07 +0300

    calc is working

---
+++ src/calculatorgui/CalculatorGUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorgui;
+
+/**
+ *
+ * @author yehud
+ */
+public class CalculatorGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculatorForm calculatorForm = new CalculatorForm();
+        calculatorForm.setVisible(true);
+    }
+    
+}
